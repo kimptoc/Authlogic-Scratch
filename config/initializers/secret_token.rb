113a1af156b0bf1f968e76fc14c5c95a2ab6b49e
Chris Kimpton <chris@kimptoc.net>
2011-05-05 17:26:07 +0100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AuthlogicScratch::Application.config.secret_token = 'b48b0bf2f939ecc5766222fdc3fb8b154c94d6130ccd3622d549a4bac2b7223ba1da1e4964e1694385d2ac0005f2083f6cf41faed16674fa1fa7786523e4ed58'
